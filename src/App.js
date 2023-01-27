2e5175c9e1943e3b55a110900d3623fd6331c8da
rakibul-wdp <rakibul.wdp@gmail.com>
2023-01-27 10:09:49 +0600

    styled component use case and setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 import About from "./About";
 import Cart from "./Cart";
 import Contact from "./Contact";
 import ErrorPage from "./ErrorPage";
+import { GlobalStyle } from "./GlobalStyle";
 import Home from "./Home";
 import Products from "./Products";
 import SingleProduct from "./SingleProduct";
 
 function App() {
+  const theme = {
+    colors: {
+      heading: "rgb(24 24 29)",
+      text: "rgba(29 ,29, 29, .8)",
+      white: "#fff",
+      black: " #212529",
+      helper: "#8490ff",
+
+      bg: "#F6F8FA",
+      footer_bg: "#0a1435",
+      btn: "rgb(98 84 243)",
+      border: "rgba(98, 84, 243, 0.5)",
+      hr: "#ffffff",
+      gradient:
+        "linear-gradient(0deg, rgb(132 144 255) 0%, rgb(98 189 252) 100%)",
+      shadow:
+        "rgba(0, 0, 0, 0.02) 0px 1px 3px 0px,rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;",
+      shadowSupport: " rgba(0, 0, 0, 0.16) 0px 1px 4px",
+    },
+    media: {
+      mobile: "768px",
+      tab: "998px",
+    },
+  };
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/singleproduct/:id" element={<SingleProduct />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <GlobalStyle />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/singleproduct/:id" element={<SingleProduct />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,14 @@
 import React from "react";
+import styled from "styled-components";
 
 const Home = () => {
-  return <div>Home</div>;
+  return <Wrapper className="test">Home</Wrapper>;
 };
+
+const Wrapper = styled.section`
+  background-color: ${({theme}) => theme.colors.bg};
+  width: 20rem;
+  height: 20rem;
+`;
 
 export default Home;
