bf22706ba4d4d435e40d6e35a11accd0f07c71a8
rakibul-wdp <rakibul.wdp@gmail.com>
2023-01-27 12:09:07 +0600

    create hero section

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,11 @@
-import React from "react";
-import styled from "styled-components";
+import HeroSection from "./components/HeroSection";
 
 const Home = () => {
-  return <Wrapper className="test">Home</Wrapper>;
-};
+  const data = {
+    name: "Chandu store"
+  }
 
-const Wrapper = styled.section`
-  background-color: ${({theme}) => theme.colors.bg};
-  width: 20rem;
-  height: 20rem;
-`;
+  return <HeroSection myData={data} />;
+};
 
 export default Home;
