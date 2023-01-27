6fedc80e3cbf9f1c382c945b15843dfbf8c0f52e
rakibul-wdp <rakibul.wdp@gmail.com>
2023-01-27 09:01:41 +0600

    create home about component and setup router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./About";
+import Cart from "./Cart";
+import Contact from "./Contact";
+import ErrorPage from "./ErrorPage";
+import Home from "./Home";
+import Products from "./Products";
+import SingleProduct from "./SingleProduct";
+
 function App() {
-  return <div>Chandu Store.</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/singleproduct/:id" element={<SingleProduct />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/Products.js
+++ src/Products.js
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 const Products = () => {
-  return <Wrapper></Wrapper>;
+  return <Wrapper>Products</Wrapper>;
 };
 
 const Wrapper = styled.section`
