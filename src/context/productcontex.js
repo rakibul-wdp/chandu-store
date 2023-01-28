180a113d359a5611029c82f1dc56ff81d424454f
rakibul-wdp <rakibul.wdp@gmail.com>
2023-01-29 04:53:39 +0600

    create simple reducer boilerplate and use reducer in context api

--- src/context/productcontex.js
+++ src/context/productcontex.js
@@ -1,22 +1,33 @@
 import axios from "axios";
-import { createContext, useContext, useEffect } from "react";
+import { createContext, useContext, useEffect, useReducer } from "react";
+import reducer from "../reducer/productReducer";
 
 const AppContext = createContext();
 
 const API = "https://api.pujakaitem.com/api/products";
 
+const initialState = {
+  isLoading: false,
+  isError: false,
+  products: [],
+  featureProducts: [],
+};
+
 const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   const getProducts = async (url) => {
     const res = await axios.get(url);
     const products = await res.data;
-    console.log(products);
   };
 
   useEffect(() => {
     getProducts(API);
   }, []);
 
-  return <AppContext.Provider>{children}</AppContext.Provider>;
+  return (
+    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
+  );
 };
 
 // custom hooks
---
+++ src/reducer/productReducer.js
@@ -0,0 +1,5 @@
+const ProductReducer = (state, action) => {
+  return state;
+};
+
+export default ProductReducer;
