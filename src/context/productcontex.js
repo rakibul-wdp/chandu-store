892e52e75790b93b5260f8d4e45cb0c19bec554e
rakibul-wdp <rakibul.wdp@gmail.com>
2023-01-29 05:08:37 +0600

    create three reducer for products

--- src/context/productcontex.js
+++ src/context/productcontex.js
@@ -17,8 +17,14 @@ const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   const getProducts = async (url) => {
-    const res = await axios.get(url);
-    const products = await res.data;
+    dispatch({ type: "SET_LOADING" });
+    try {
+      const res = await axios.get(url);
+      const products = await res.data;
+      dispatch({ type: "SET_API_DATA", payload: products });
+    } catch (error) {
+      dispatch({ type: "API_ERROR" });
+    }
   };
 
   useEffect(() => {
