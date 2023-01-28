f26d115593bfb2c51436150ea812fd4a97d2852b
rakibul-wdp <rakibul.wdp@gmail.com>
2023-01-29 04:43:04 +0600

    fetch api by axios and get products data

--- src/context/productcontex.js
+++ src/context/productcontex.js
@@ -1,8 +1,21 @@
-import { createContext, useContext } from "react";
+import axios from "axios";
+import { createContext, useContext, useEffect } from "react";
 
 const AppContext = createContext();
 
+const API = "https://api.pujakaitem.com/api/products";
+
 const AppProvider = ({ children }) => {
+  const getProducts = async (url) => {
+    const res = await axios.get(url);
+    const products = await res.data;
+    console.log(products);
+  };
+
+  useEffect(() => {
+    getProducts(API);
+  }, []);
+
   return <AppContext.Provider>{children}</AppContext.Provider>;
 };
 
