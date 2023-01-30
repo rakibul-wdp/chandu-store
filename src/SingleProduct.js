608b6dafdc65f12fa31eb68459f45878fb49b289
rakibul-wdp <rakibul.wdp@gmail.com>
2023-01-30 10:41:14 +0600

    create my image component properly

--- src/SingleProduct.js
+++ src/SingleProduct.js
@@ -76,7 +76,7 @@ const SingleProduct = () => {
 
               <div className="product-warranty-data">
                 <TbTruckDelivery className="warranty-icon" />
-                <p>Thapa Delivered </p>
+                <p>Chandu Delivered </p>
               </div>
 
               <div className="product-warranty-data">
