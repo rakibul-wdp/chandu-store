0f99ab0dd91b5f65fe75e8bec62f34cac77e9e18
rakibul-wdp <rakibul.wdp@gmail.com>
2023-01-30 17:18:11 +0600

    add more functionality in fiter reducer file

--- src/reducer/filterReducer.js
+++ src/reducer/filterReducer.js
@@ -13,6 +13,49 @@ const filterReducer = (state, action) => {
         grid_view: true,
       };
 
+    case "SET_LIST_VIEW":
+      return {
+        ...state,
+        grid_view: false,
+      };
+
+    case "GET_SORT_VALUE":
+      return {
+        ...state,
+        sorting_value: action.payload,
+      };
+
+    case "SORTING_PRODUCTS":
+      let newSortData;
+
+      const { filter_products, sorting_value } = state;
+      let tempSortProduct = [...filter_products];
+
+      const sortingProducts = (a, b) => {
+        if (sorting_value === "lowest") {
+          return a.price - b.price;
+        }
+
+        if (sorting_value === "highest") {
+          return b.price - a.price;
+        }
+
+        if (sorting_value === "a-z") {
+          return a.name.localeCompare(b.name);
+        }
+
+        if (sorting_value === "z-a") {
+          return b.name.localeCompare(a.name);
+        }
+      };
+
+      newSortData = tempSortProduct.sort(sortingProducts);
+
+      return {
+        ...state,
+        filter_products: newSortData,
+      };
+
     default:
       return state;
   }
