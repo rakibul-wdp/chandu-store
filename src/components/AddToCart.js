373322646a5c617381e8fb64dcb7962283dfabe4
rakibul-wdp <rakibul.wdp@gmail.com>
2023-01-31 13:51:07 +0600

    create cart context for cart

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -2,10 +2,12 @@ import { useState } from "react";
 import { FaCheck } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
+import { useCartContext } from "../context/cart_context";
 import { Button } from "../styles/Button";
 import CartAmountToggle from "./CartAmountToggle";
 
 const AddToCart = ({ product }) => {
+  const { addToCart } = useCartContext();
   const { id, colors, stock } = product;
 
   const [color, setColor] = useState(colors[0]);
@@ -46,7 +48,7 @@ const AddToCart = ({ product }) => {
         setIncrease={setIncrease}
       />
 
-      <NavLink to="/cart">
+      <NavLink to="/cart" onClick={() => addToCart(id, color, amount, product)}>
         <Button className="btn">Add To Cart</Button>
       </NavLink>
     </Wrapper>
