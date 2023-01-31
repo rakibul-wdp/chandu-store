fd668008ec27a4e268b72438290db33d87251f61
rakibul-wdp <rakibul.wdp@gmail.com>
2023-01-31 13:03:38 +0600

    add price filter ui

--- src/components/FilterSection.js
+++ src/components/FilterSection.js
@@ -1,12 +1,15 @@
 import { FaCheck } from "react-icons/fa";
 import styled from "styled-components";
 import { useFilterContext } from "../context/filter_context";
+import FormatPrice from "../Helpers/FormatPrice";
+import { Button } from "../styles/Button";
 
 const FilterSection = () => {
   const {
-    filters: { text, category, color },
+    filters: { text, category, color, price, maxPrice, minPrice },
     updateFilterValue,
     all_products,
+    clearFilters,
   } = useFilterContext();
 
   // get the unique values of each property
@@ -116,6 +119,27 @@ const FilterSection = () => {
             );
           })}
         </div>
+      </div>
+
+      <div className="filter_price">
+        <h3>Price</h3>
+        <p>
+          <FormatPrice price={price} />
+        </p>
+        <input
+          type="range"
+          name="price"
+          min={minPrice}
+          max={maxPrice}
+          value={price}
+          onChange={updateFilterValue}
+        />
+      </div>
+
+      <div className="filter-clear">
+        <Button className="btn" onClick={clearFilters}>
+          Clear Filters
+        </Button>
       </div>
     </Wrapper>
   );
